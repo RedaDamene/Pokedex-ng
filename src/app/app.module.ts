39af97cea78942c5d6e28db2fa786d83a521c220
Reda Damene <90632620+RedaDamene@users.noreply.github.com>
2023-07-27 13:43:16 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GenerationsComponent } from './components/generations/generations.component';
+import { PokemonsComponent } from './components/pokemons/pokemons.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GenerationsComponent,
+    PokemonsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
